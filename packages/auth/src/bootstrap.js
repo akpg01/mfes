387b07cc60f58543788f8f0a766cc7fa53409537
Grace Akpan <opps4lrng@gmail.com>
2021-07-05 00:40:13 -0600

    implemented multi-tier nav and performance enhancements

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -6,10 +6,7 @@ import App from "./App";
 // Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   const history =
-    defaultHistory ||
-    createMemoryHistory({
-      initialEntries: [initialPath],
-    });
+    defaultHistory || createMemoryHistory({ initialEntries: [initialPath] });
 
   // whenever navigation occurs, call function
   if (onNavigate) {
@@ -31,7 +28,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 
 // If we are in development and in isolation, call mout immediately
 if (process.env.NODE_ENV === "development") {
-  const devRoot = document.querySelector("#_marketing-dev-root");
+  const devRoot = document.querySelector("#_auth-dev-root");
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,12 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import MarketingApp from "./components/MarketingApp";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Progress from "./components/Progress";
 import Header from "./components/Header";
+
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
+
 import {
   StylesProvider,
   createGenerateClassName,
@@ -15,7 +20,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <MarketingApp />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
